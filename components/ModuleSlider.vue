356365ed28c6f6ccc7cb4d3c1b83745d0c7570b0
gckohaku <cyanhairneko@gmail.com>
2024-12-20 16:33:26 +0900

    feature: UI の矢印ボタンが機能するようにする (#22)

--- components/ModuleSlider.vue
+++ components/ModuleSlider.vue
@@ -9,18 +9,54 @@ interface Props {
 	min?: string | number;
 	max?: string | number;
 	step?: string | number;
+	isRolling?: boolean
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+	min: 0,
+	max: 100,
+	step: 1,
+	isRolling: false,
+}); 
+
+const onPushLeftButton = async () => {
+	if (!props.isRolling && Number(modelValue.value) <= Number(props.min)) {
+		return;
+	}
+
+	modelValue.value = Number(modelValue.value) - Number(props.step);
+
+	await nextTick();
+
+	if (modelValue.value && props.isRolling && Number(modelValue.value) < Number(props.min)) {
+		const afterRollingValue: number = Number(modelValue.value) + (Number(props.max) - Number(props.min));
+		modelValue.value = afterRollingValue;
+	}
+}
+
+const onPushRightButton = async () => {
+	if (!props.isRolling && Number(modelValue.value) >= Number(props.max)) {
+		return;	
+	}
+	
+	modelValue.value = Number(modelValue.value) + Number(props.step);
+
+	await nextTick();
+
+	if (modelValue.value && props.isRolling && Number(modelValue.value) > Number(props.max)) {
+		const afterRollingValue: number = Number(modelValue.value) - (Number(props.max) - Number(props.min));
+		modelValue.value = afterRollingValue;
+	}
+}
 </script>
 
 <template>
 	<div class="module-wrapper">
 		<p>{{ text }}: {{modelValue}}</p>
 		<div class="slider-container">
-			<button><</button>
+			<button @click="onPushLeftButton"><</button>
 			<input type="range" :name="name" :id="id" :min="min" :max="max" :value="modelValue" :step="step" @input="(e) => modelValue = (e.target as HTMLInputElement).value">
-			<button>></button>
+			<button @click="onPushRightButton">></button>
 		</div>
 	</div>
 </template>
--- components/Rendering4D.vue
+++ components/Rendering4D.vue
@@ -246,12 +246,12 @@ onMounted(() => {
 			<ModuleSlider text="y" max="300" min="-300" v-model="moveY" />
 			<ModuleSlider text="z" max="300" min="-300" v-model="moveZ" />
 			<ModuleSlider text="w" max="300" min="-300" v-model="moveW" />
-			<ModuleSlider text="rotateXW" max="360" min="-360" v-model="rotateXW" />
-			<ModuleSlider text="rotateYW" max="360" min="-360" v-model="rotateYW" />
-			<ModuleSlider text="rotateZW" max="360" min="-360" v-model="rotateZW" />
-			<ModuleSlider text="rotateXY" max="360" min="-360" v-model="rotateXY" />
-			<ModuleSlider text="rotateYZ" max="360" min="-360" v-model="rotateYZ" />
-			<ModuleSlider text="rotateXZ" max="360" min="-360" v-model="rotateXZ" />
+			<ModuleSlider text="rotateXW" max="360" min="-360" v-model="rotateXW" :isRolling="true" />
+			<ModuleSlider text="rotateYW" max="360" min="-360" v-model="rotateYW" :isRolling="true" />
+			<ModuleSlider text="rotateZW" max="360" min="-360" v-model="rotateZW" :isRolling="true" />
+			<ModuleSlider text="rotateXY" max="360" min="-360" v-model="rotateXY" :isRolling="true" />
+			<ModuleSlider text="rotateYZ" max="360" min="-360" v-model="rotateYZ" :isRolling="true" />
+			<ModuleSlider text="rotateXZ" max="360" min="-360" v-model="rotateXZ" :isRolling="true" />
 			<ModuleSlider text="sizeX" max="2.0" min="0.1" step="0.1" v-model="sizeX" />
 			<ModuleSlider text="sizeY" max="2.0" min="0.1" step="0.1" v-model="sizeY" />
 			<ModuleSlider text="sizeZ" max="2.0" min="0.1" step="0.1" v-model="sizeZ" />
